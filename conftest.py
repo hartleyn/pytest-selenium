ff5cedd80a684627de1872c70ecb5ce10ba7e829
hartleyn <nhartley18@me.com>
2019-06-05 10:04:48 -0400

    Adding a comment to conftest.py

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 @pytest.fixture(scope="module")
 def chrome_driver(request):
     driver = webdriver.Chrome()
-
+    # Shutdown the webdriver instance after tests finish
     request.addfinalizer(lambda *args: driver.quit())
 
     return driver
