2cebdb0e69c3a497824ba031747d666d5220481f
hartleyn <nhartley18@me.com>
2019-06-05 10:28:07 -0400

    Removing double quotes in conftest.py

--- conftest.py
+++ conftest.py
@@ -2,10 +2,10 @@ import pytest
 from selenium import webdriver
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope='module')
 def chrome_driver(request):
     driver = webdriver.Chrome()
-    # Shutdown the webdriver instance after tests finish
+    # Shutdown the webdriver instance upon completion of test run
     request.addfinalizer(lambda *args: driver.quit())
 
     return driver
